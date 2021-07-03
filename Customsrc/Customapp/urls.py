cb349677c2194b6da835541b8c3aa9c85cba24f5
coder-sanju <sanju2help@gmail.com>
2021-07-03 13:35:44 +0530

    login logout done

--- Customsrc/Customapp/urls.py
+++ Customsrc/Customapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import UserProfileListView , UserProfileCreateView, UserProfileUpdateView ,UserProfileDeleteView
-
-
+from .views import UserProfileListView , UserProfileCreateView, UserProfileUpdateView ,UserProfileDeleteView,LoginView,LogoutView
+from django.contrib.auth.decorators import login_required
 urlpatterns = [
-    path('', UserProfileListView.as_view(), name="userprofileview"),
-    path('add-user/',UserProfileCreateView.as_view(),name='userprofilecreate'),
-    path('user/<int:pk>/update',UserProfileUpdateView.as_view(),name='userprofileupdate'),
-    path('user/<int:pk>/delete',UserProfileDeleteView.as_view(),name='userprofiledelete')
+    path('',LoginView.as_view(),name='login'),
+    path('logout/',login_required(LogoutView.as_view()),name="logout"),
+    path('user-list/', login_required(UserProfileListView.as_view()), name="userprofileview"),
+    path('add-user/',login_required(UserProfileCreateView.as_view()),name='userprofilecreate'),
+    path('user/<int:pk>/update/',login_required(UserProfileUpdateView.as_view()),name='userprofileupdate'),
+    path('user/<int:pk>/delete/',login_required(UserProfileDeleteView.as_view()),name='userprofiledelete')
 ]
--- Customsrc/Customsrc/settings.py
+++ Customsrc/Customsrc/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Customsrc.wsgi.application'
 
-
+LOGIN_URL = '/'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
